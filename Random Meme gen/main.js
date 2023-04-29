f1de2201bb5cd0c0b673585cdf3e747b5df4d08c
og-JND <69082025+og-JND@users.noreply.github.com>
2023-04-29 22:57:46 +0100

    added the subreddit selection feature!

--- Random Meme gen/main.js
+++ Random Meme gen/main.js
@@ -1,9 +1,17 @@
-const apiLink = "https://meme-api.com/gimme";
+let apiLink = "https://meme-api.com/gimme";
 const memeButton = document.querySelector(".generateMemeButton");
 const memeAuthor = document.querySelector(".memeAuthor");
 const memeTitle = document.querySelector(".memeTitle");
-const memeImg = document.querySelector(".memeGen img")
-
+const memeImg = document.querySelector(".memeGen img");
+const subredditOptionList = document.querySelectorAll("li");
+console.log(subredditOptionList[1].innerHTML)
+const subreddits = {
+    1 : "wholesomememes",
+    2 : "dankmemes",
+    3 : "me_irl",
+    4 : "memes",
+    5 : "jojomemes",
+}
 function updateDetails(url, title, author, subreddit){
     memeImg.setAttribute("src", url);
     memeTitle.innerHTML = title;
@@ -11,10 +19,26 @@ function updateDetails(url, title, author, subreddit){
 }
 
 function generateMeme() {
-    fetch("https://meme-api.com/gimme").then(
+    fetch(apiLink).then(
         (response) => response.json()).then(
             (data) => {
                 updateDetails(data.url, data.title, data.author, data.subreddit);
             })
 }
+function editApiLink(params) {
+    if (apiLink = "https://meme-api.com/gimme"){
+        apiLink = apiLink+"/"+params;
+    }else{
+        apiLink = "https://meme-api.com/gimme/"+params;
+    }
+    
+    console.log(apiLink);
+}
 memeButton.addEventListener("click", generateMeme);
+
+for (let i = 0; i < subredditOptionList.length; i++) {
+    const element = subredditOptionList[i];
+    element.addEventListener("click", function (params) {
+        editApiLink(element.innerHTML);    
+    })
+}
